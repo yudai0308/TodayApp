ca42e94cdc20db4220d1d6ee40e96ffd8a3bb182
yudai0308 <lock0308@gmail.com>
2022-05-28 15:33:38 +0900

    Initial Commit

---
+++ TodayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodayApp
+//
+//  Created by Watanabe Yudai on 2022/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
